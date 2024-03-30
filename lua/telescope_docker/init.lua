b3d555621685474aa1e4a6393714aef3c7fed45f
Kris Jenkins <krisajenkins@gmail.com>
2024-03-30 10:15:56 +0000

    Adding docker_ps and docker_volumes.

--- lua/telescope_docker/init.lua
+++ lua/telescope_docker/init.lua
@@ -24,6 +24,122 @@ M._make_docker_command = function(args)
     return job
 end
 
+M.docker_ps = function(opts)
+    pickers
+        .new(opts, {
+            finder = finders.new_dynamic({
+                fn = function()
+                    return M._make_docker_command { 'ps' }
+                end,
+
+                entry_maker = function(entry)
+                    log.info('Got entry', entry)
+                    local process = vim.json.decode(entry)
+                    log.info('Got entry', process)
+                    if process then
+                        return {
+                            value = process,
+                            display = process.Names,
+                            ordinal = process.ID .. ' ' .. process.Names,
+                        }
+                    end
+                end,
+            }),
+
+            sorter = conf.generic_sorter(opts),
+
+            previewer = previewers.new_buffer_previewer({
+                title = 'Process Details',
+                define_preview = function(self, entry)
+                    local formatted = {
+                        '# ID: ' .. entry.value.ID,
+                        '',
+                        '*Names*: ' .. entry.value.Names,
+                        '*Command*: ' .. entry.value.Command,
+                        '*Labels*: ' .. entry.value.Labels,
+                        '',
+                        '*Image*: ' .. entry.value.Image,
+                        '*LocalVolumes*: ' .. entry.value.LocalVolumes,
+                        '*Mounts*: ' .. entry.value.Mounts,
+                        '*Networks*: ' .. entry.value.Networks,
+                        '*Ports*: ' .. entry.value.Ports,
+                        '',
+                        '*Size*: ' .. entry.value.Size,
+                        '',
+                        '*State*: ' .. entry.value.State,
+                        '*Status*: ' .. entry.value.Status,
+                        '*CreatedAt*: ' .. entry.value.CreatedAt,
+                        '*RunningFor*: ' .. entry.value.RunningFor,
+                    }
+
+                    vim.api.nvim_buf_set_lines(self.state.bufnr, 0, 0, true, formatted)
+                    utils.highlighter(self.state.bufnr, 'markdown')
+                end,
+            }),
+            attach_mappings = function(prompt_bufnr)
+                actions.select_default:replace(function()
+                    actions.close(prompt_bufnr)
+                end)
+                return true
+            end,
+        })
+        :find()
+end
+
+M.docker_volumes = function(opts)
+    pickers
+        .new(opts, {
+            finder = finders.new_dynamic({
+                fn = function()
+                    return M._make_docker_command { 'volume', 'ls' }
+                end,
+
+                entry_maker = function(entry)
+                    local volume = vim.json.decode(entry)
+                    log.info('Calling entry maker', volume)
+                    if volume then
+                        return {
+                            value = volume,
+                            display = volume.Name,
+                            ordinal = volume.Name,
+                        }
+                    end
+                end,
+            }),
+
+            sorter = conf.generic_sorter(opts),
+
+            previewer = previewers.new_buffer_previewer({
+                title = 'Volume Details',
+                define_preview = function(self, entry)
+                    local formatted = {
+                        '# ' .. entry.display,
+                        '',
+                        '*Labels*: ' .. entry.value.Labels,
+                        '*Availability*: ' .. entry.value.Availability,
+                        '*Driver*: ' .. entry.value.Driver,
+                        '*Group*: ' .. entry.value.Group,
+                        '*Links*: ' .. entry.value.Links,
+                        '*Scope*: ' .. entry.value.Scope,
+                        '*Size*: ' .. entry.value.Size,
+                        '*Status*: ' .. entry.value.Status,
+                        '*Mountpoint*: ' .. entry.value.Mountpoint,
+                    }
+                    vim.api.nvim_buf_set_lines(self.state.bufnr, 0, 0, true, formatted)
+
+                    utils.highlighter(self.state.bufnr, 'markdown')
+                end,
+            }),
+            attach_mappings = function(prompt_bufnr)
+                actions.select_default:replace(function()
+                    actions.close(prompt_bufnr)
+                end)
+                return true
+            end,
+        })
+        :find()
+end
+
 M.docker_images = function(opts)
     pickers
         .new(opts, {
